6c010769fba42a8a861a38a685a4d8277a6b3a24
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-16 17:49:02 +0530

    refactor: add NativeSphere typography system

---
+++ src/theme/typography.js
@@ -0,0 +1,36 @@
+export const typography = {
+    heading1: {
+        fontSize: 30,
+        fontWeight: '800',
+    },
+
+    heading2: {
+        fontSize: 24,
+        fontWeight: '800',
+    },
+
+    heading3: {
+        fontSize: 18,
+        fontWeight: '700',
+    },
+
+    body: {
+        fontSize: 14,
+        fontWeight: '400',
+    },
+
+    bodyMedium: {
+        fontSize: 14,
+        fontWeight: '500',
+    },
+
+    caption: {
+        fontSize: 12,
+        fontWeight: '400',
+    },
+
+    button: {
+        fontSize: 14,
+        fontWeight: '700',
+    },
+};
