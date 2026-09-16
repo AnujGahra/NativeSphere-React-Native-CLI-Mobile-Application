11aa5d8cbd0eb10b24934f17e8a28c23ad532bfe
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-16 17:48:28 +0530

    refactor: centralize NativeSphere application strings

---
+++ src/constants/strings.js
@@ -0,0 +1,15 @@
+export const APP_NAME = 'NativeSphere';
+
+export const APP_TAGLINE =
+    'Explore the Native Capabilities of Your Device';
+
+export const APP_VERSION = '1.0.0';
+
+export const FEATURES = {
+    NETWORK: 'Network',
+    CAMERA: 'Camera',
+    AUDIO: 'Audio',
+    LOCATION: 'Location',
+    DEVICE: 'Device Information',
+    PERMISSIONS: 'Permissions',
+};
