05458f655b5907deac838df3234498b2a1b5e266
Anuj Kumar <anujgahra244304@gmail.com>
2026-09-16 17:49:52 +0530

    refactor: centralize permission constants

---
+++ src/constants/permissions.js
@@ -0,0 +1,11 @@
+export const PERMISSION_KEYS = {
+    CAMERA: 'camera',
+    MICROPHONE: 'microphone',
+    LOCATION: 'location',
+};
+
+export const PERMISSION_LABELS = {
+    camera: 'Camera',
+    microphone: 'Microphone',
+    location: 'Location',
+};
